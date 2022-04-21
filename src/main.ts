4374c4c5b573937b4ea0c1cfbb75c36e2dd59a56
kasparas-ban <kasparas.baniulis@gmail.com>
2022-04-21 17:14:52 +0300

    Added Typescript

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-const fs = require("fs");
-const puppeteer = require("puppeteer");
-const userAgent = require("user-agents");
+import fs from "fs";
+import puppeteer from "puppeteer";
+import userAgent from "user-agents";
+
+interface Listing {
+  district: string,
+  street: string,
+  price: string,
+  pricePM: string,
+  roomNum: number,
+  area: number,
+  floor: string,
+  link?: string,
+}
 
 (async function main() {
   const browser = await puppeteer.launch();
@@ -15,16 +26,18 @@ const userAgent = require("user-agents");
   const city = ["vilniuje", "kaune"];
   const selection = { type: listingType[0], city: city[0] };
   const mainUrl = `https://www.aruodas.lt/${selection.type}/${selection.city}/puslapis/1/?FOrder=AddDate`;
-  const pagesNum = await getPagesNum(page, mainUrl);
-  let listings = [];
+  const maxPageNum = await getPagesNum(page, mainUrl);
+  const pages = [...Array(maxPageNum).keys()].slice(1);
+  let listings: Listing[] = [];
 
-  for (let num of [1, 2, 3]) {
+  for (const num of pages) {
     await page.waitForTimeout(1000);
     const pageListings = await getListingsFromPage(
       page,
       `https://www.aruodas.lt/${selection.type}/${selection.city}/puslapis/${num}/?FOrder=AddDate`
     );
     listings = listings.concat(pageListings);
+    console.log(`Page ${num} is scrapped.`);
   }
 
   saveListingsToFile(listings);
@@ -32,43 +45,47 @@ const userAgent = require("user-agents");
   await browser.close();
 })();
 
-function saveListingsToFile(listings) {
+function saveListingsToFile(listings: Listing[]) {
   const jsonData = JSON.stringify(listings);
   fs.writeFile("aruodasData.json", jsonData, "utf8", () =>
     console.log("Data written to file successfully.")
   );
 }
 
-async function getPagesNum(page, url) {
+async function getPagesNum(page: puppeteer.Page, url: string) {
   await page.setUserAgent(userAgent.toString());
   await page.goto(url, { timeout: 300000 });
 
   const numOfPages = await page.evaluate(() => {
     const selectionElements = Array.from(document.querySelectorAll(".page-bt"));
-    return Number(selectionElements[selectionElements.length - 2].innerText);
+    return Number(
+      (selectionElements[selectionElements.length - 2] as HTMLElement).innerText
+    );
   });
 
   return numOfPages;
 }
 
-async function getListingsFromPage(page, url) {
+async function getListingsFromPage(page: puppeteer.Page, url: string) {
   await page.setUserAgent(userAgent.toString());
   await page.goto(url, { timeout: 300000 });
 
   const listings = await page.evaluate(() => {
-    let results = [];
-    let listing = Array.from(document.querySelectorAll(".list-row")).filter(
+    const results: Listing[] = [];
+    const listing = Array.from(document.querySelectorAll(".list-row")).filter(
       (li) => !!li.querySelector(".list-RoomNum")
     );
     listing.forEach((item) => {
-      const address = item.querySelector("h3").innerText.split("\n");
+      const address = (item.querySelector("h3") as HTMLElement).innerText.split("\n");
       const district = address[0];
       const street = address[1];
-      const price = item.querySelector(".list-item-price").innerText;
-      const pricePM = item.querySelector(".price-pm").innerText;
-      const roomNum = Number(item.querySelector(".list-RoomNum").innerText);
-      const area = Number(item.querySelector(".list-AreaOverall").innerText);
-      const floor = item.querySelector(".list-Floors").innerText;
+      const price = (item.querySelector(".list-item-price") as HTMLElement).innerText;
+      const pricePM = (item.querySelector(".price-pm") as HTMLElement).innerText;
+      const roomNum = Number((item.querySelector(".list-RoomNum") as HTMLElement).innerText);
+      const area = Number((item.querySelector(".list-AreaOverall") as HTMLElement).innerText);
+      const floor = (item.querySelector(".list-Floors") as HTMLElement).innerText;
+      const link = (item.querySelector("h3 a") as HTMLAnchorElement | null)?.href;
+
       results.push({
         district,
         street,
@@ -77,6 +94,7 @@ async function getListingsFromPage(page, url) {
         roomNum,
         area,
         floor,
+        link,
       });
     });
     return results;
